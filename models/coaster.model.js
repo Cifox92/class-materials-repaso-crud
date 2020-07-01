cc7ae2887a4c98cb8709405b1b904f19d6172383
David <drgomez92@gmail.com>
2020-07-01 23:51:13 +0200

    Repaso CRUD: FINALIZADO sin bonus.

--- models/coaster.model.js
+++ models/coaster.model.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// Aquí el esquema
+const coasterSchema = new Schema({
+    name: String,
+    description: String,
+    inversions: Number,
+    length: Number,
+    active: {
+        type: Boolean,
+        default: true
+    },
+    park: {
+        type: Schema.Types.ObjectId, 
+        ref: 'Park'
+    }
+}, {
+    timestamps: true
+})
 
 module.exports = mongoose.model('Coaster', coasterSchema)
